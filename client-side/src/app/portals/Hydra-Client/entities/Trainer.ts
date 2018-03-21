d57213557e0a7310637eeae8e8f676d1cb410d44
BrandonC <brandoncross@live.com>
2018-03-21 13:06:14 -0400

    populate and change current

---
+++ client-side/src/app/portals/Hydra-Client/entities/Trainer.ts
@@ -0,0 +1,9 @@
+export class Trainer {
+    public static ROLE_INACTIVE = 'ROLE_INACTIVE';
+
+    trainerId: number;
+    name: string;
+    title: string;
+    email: string;
+    tier: string;
+}
--- client-side/src/app/portals/Hydra-Client/services/trainer/trainer.service.ts
+++ client-side/src/app/portals/Hydra-Client/services/trainer/trainer.service.ts
@@ -1,8 +1,57 @@
 import { Injectable } from '@angular/core';
 
+// entities
+import { Trainer } from '../../../../entities/Trainer';
+
+
+
+// rxjs
+import { Observable } from 'rxjs/Observable';
+import { UrlService } from '../url/url.service';
+import { HttpClient } from '@angular/common/http';
+
+
 @Injectable()
 export class TrainerService {
 
-  constructor() { }
+  public listObservable = new Observable<Trainer[]>(null);
+  public titlesObservable = new Observable<String[]>();
+  public tiersObservable = new Observable<String[]>();
+  // public currentTrainer = new Observable<Trainer>(null);
+  public currentTrainer = new Trainer;
+
+
+  constructor(private httpClient: HttpClient, private urls: UrlService) { }
+
+
+public populateOnStart(): void {
+  this.httpClient.get<String[]>(this.urls.trainers.getTitles()).subscribe(succ => this.titlesObservable);
+  this.httpClient.get<String[]>(this.urls.trainers.getRoles()).subscribe(succ => this.tiersObservable);
+ this.fetchAll();
+}
+
+/**
+  *
+  * sets current trainer
+  */
+
+public changeCurrentTrainer(trainer: Trainer) {
+  this.currentTrainer = trainer;
+}
+
+
+
+  /*
+    =====================
+    BEGIN: API calls
+    =====================
+  */
+
+
+
+public fetchAll(): Observable<Trainer[]> {
+  this.httpClient.get<Trainer[]>(this.urls.trainers.fetchAll()).subscribe(succ => this.listObservable);
+  return this.listObservable;
+}
 
 }
