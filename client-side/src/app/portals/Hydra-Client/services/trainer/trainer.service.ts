9f7c38fb639d5613e3397cdd95723efa05625317
BrandonC <brandoncross@live.com>
2018-03-21 13:37:00 -0400

    trainer service api

--- client-side/src/app/portals/Hydra-Client/services/trainer/trainer.service.ts
+++ client-side/src/app/portals/Hydra-Client/services/trainer/trainer.service.ts
@@ -17,27 +17,27 @@ export class TrainerService {
   public listObservable = new Observable<Trainer[]>(null);
   public titlesObservable = new Observable<String[]>();
   public tiersObservable = new Observable<String[]>();
-  // public currentTrainer = new Observable<Trainer>(null);
-  public currentTrainer = new Trainer;
+  public currentTrainer = new Observable<Trainer>(null);
+  // public currentTrainer = new Trainer;
 
 
   constructor(private httpClient: HttpClient, private urls: UrlService) { }
 
 
-public populateOnStart(): void {
-  this.httpClient.get<String[]>(this.urls.trainers.getTitles()).subscribe(succ => this.titlesObservable);
-  this.httpClient.get<String[]>(this.urls.trainers.getRoles()).subscribe(succ => this.tiersObservable);
- this.fetchAll();
-}
+// public populateOnStart(): void {
+//   this.httpClient.get<String[]>(this.urls.trainers.getTitles()).subscribe(succ => this.titlesObservable);
+//   this.httpClient.get<String[]>(this.urls.trainers.getRoles()).subscribe(succ => this.tiersObservable);
+//  this.fetchAll();
+// }
 
 /**
   *
-  * sets current trainer
+  * sets current trainer | We didn't need this
   */
 
-public changeCurrentTrainer(trainer: Trainer) {
-  this.currentTrainer = trainer;
-}
+// public changeCurrentTrainer(trainer: Trainer) {
+//   return this.httpClient.get<Trainer>(this.urls.trainers.fetch)
+// }
 
 
 
@@ -47,11 +47,49 @@ public changeCurrentTrainer(trainer: Trainer) {
     =====================
   */
 
-
+ /**
+    * retrieves all trainers and pushes them on the
+    * list subject
+    *
+    * spring-security: @PreAuthorize("hasAnyRole('VP', 'TRAINER', 'STAGING', 'QC', 'PANEL')")
+    */
 
 public fetchAll(): Observable<Trainer[]> {
   this.httpClient.get<Trainer[]>(this.urls.trainers.fetchAll()).subscribe(succ => this.listObservable);
   return this.listObservable;
 }
+
+public fetchByEmail(email: string) {
+ return this.httpClient.get<Trainer>(this.urls.trainers.fetchByEmail(email)).subscribe();
+}
+
+/**
+   * creates a trainer and pushes the created trainer on the
+   * savedSubject
+   *
+   * spring-security: @PreAuthorize("hasAnyRole('VP')")
+   *
+   * @param trainer: Trainer
+   */
+
+public create(trainer: Trainer): Observable<Trainer> {
+  return this.httpClient.post<Trainer>(this.urls.trainers.save(), trainer);
+}
+
+/**
+   * updates a trainer and pushes the updated trainer on the
+   * savedSubject
+   *
+   * spring-security: @PreAuthorize("hasAnyRole('VP')")
+   *
+   * @param trainer: Trainer
+   */
+
+public update(trainer: Trainer): Observable<Trainer> {
+  return this.httpClient.put<Trainer>(this.urls.trainers.update(), trainer);
+}
+
+
+
 
 }
