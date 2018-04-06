0272572de2e4bcbbf8e17c5f32e34427a5cf319e
Jennifer Fox <jrufox@gmail.com>
2018-04-05 23:49:09 -0400

    added navbar fix

--- client-side/src/app/portals/Track-Force/testing-helpers/router-stubs.ts
+++ client-side/src/app/portals/Track-Force/testing-helpers/router-stubs.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // export for convenience.
 /*
    Code found at https://angular.io/guide/testing#live-examples
@@ -54,63 +53,6 @@ export class ActivatedRouteStub {
     return { paramMap: this.testParamMap };
   }
 }
-=======
- // export for convenience.
- /*
-    Code found at https://angular.io/guide/testing#live-examples
- */
- export { ActivatedRoute, Router, RouterLink, RouterOutlet} from '@angular/router';
-
- import { Component, Directive, Injectable, Input } from '@angular/core';
- import { NavigationExtras } from '@angular/router';
-
- @Directive({
-   selector: '[routerLink]',
-   host: {
-     '(click)': 'onClick()'
-   }
- })
- export class RouterLinkStubDirective {
-   @Input() linkParams: string;
-   navigatedTo: any = null;
-   onClick() {
-     this.navigatedTo = this.linkParams;
-   }
- }
-
- @Component({selector: 'router-outlet', template: ''})
- export class RouterOutletStubComponent { }
-
- @Injectable()
- export class RouterStub {
-   navigate(commands: any[], extras?: NavigationExtras) { }
- }
-
-
- // Only implements params and part of snapshot.paramMap
- import { BehaviorSubject } from 'rxjs/BehaviorSubject';
- import { convertToParamMap, ParamMap } from '@angular/router';
-
- @Injectable()
- export class ActivatedRouteStub {
-
-   // ActivatedRoute.paramMap is Observable
-   private subject = new BehaviorSubject(convertToParamMap(this.testParamMap));
-   paramMap = this.subject.asObservable();
-
-   // Test parameters
-   private _testParamMap: ParamMap;
-   get testParamMap() { return this._testParamMap; }
-   set testParamMap(params: {}) {
-     this._testParamMap = convertToParamMap(params);
-     this.subject.next(this._testParamMap);
-   }
-   // ActivatedRoute.snapshot.paramMap
-   get snapshot() {
-     return { paramMap: this.testParamMap };
-   }
- }
->>>>>>> master
 
 
  /*
