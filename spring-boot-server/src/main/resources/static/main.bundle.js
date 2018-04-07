c0f3367d758762607b1ffd327da8d7ba3d9624d2
Justin Joseph <josephjustn@gmail.com>
2018-04-07 02:03:24 -0400

    Changed context to localhost

--- spring-boot-server/src/main/resources/static/main.bundle.js
+++ spring-boot-server/src/main/resources/static/main.bundle.js
@@ -7107,14 +7107,14 @@ module.exports = module.exports.toString();
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-var context = 'http://localhost:8909/';
-var bam = 'http://18.219.59.193:9001/api/v2';
+var context = 'http://localhost:9001/api/v2';
+var bam = 'http://localhost:9001/api/v2';
 var environment = {
     production: false,
-    hydraContext: 'http://10.226.124.149:8765/',
+    hydraContext: 'http://localhost:8765/',
     context: context,
     bam: bam,
-    url: 'http://localhost:8080/',
+    url: 'http://localhost:80/',
     assessment: {
         fetchByBatchIdByWeek: function (batchId, week) { return context + "trainer/assessment/" + batchId + "/" + week; },
         save: function () { return context + "trainer/assessment/create"; },
